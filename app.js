50b2e900cfe4d89fcbc30130bf42047f96a764dd
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-01-31 10:21:25 +0100

    ok

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ var express = require("express");
 var app = express();
 var request = require("request");
 request({
-url: "https://blockchain.info/stats?format=json",
+
+url: "https://blockchain.info/rawaddr/1AmEJ8gZyx2TxcSHPEbLWG8onzRvp2Qn7M?format=json",
 json: true 
 },function(error,response,body){
-    price = body.market_price_usd
+    balance = body.final_balance;
+    totalRecieved = body.total_received;
+    totalSent = body.total_sent;
+    totalSent = body.total_sent;
 });
+//app.get("/index",function(req,res){
+ //   res.sendFile(__dirname +"/index.html");
+//});
+
 app.get("/",function(req,res){
-    console.log("Single Handedly Handling Myself");
-    res.send("Bitcoin Latest Price" + price);
+    res.send("Address Balance " + total_received);
 });
+app.get("overview",function(req,res){
+
+})
+//server is listening
 app.listen(80,function(){
     console.log("Ose se Ye Mi nio");
 });
